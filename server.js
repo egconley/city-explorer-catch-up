521cc2c5a776f7b81da33e46eef68ad3a784b5b3
egconley <31393715+egconley@users.noreply.github.com>
2019-11-08 19:33:58 -0800

    working yelp, not working movies

--- server.js
+++ server.js
@@ -20,20 +20,21 @@ client.on('err', err => { throw err; });
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
 app.get('/location_table', tableHandler);
+app.get('/yelp', yelpHandler);
+app.get('', movieHandler);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
+///LOCATIONS
 let locations = {};
 
 function locationHandler(request,response) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
   console.log(locations[url]);
   if ( locations[url] ) {
-    console.log(url);
     response.send(locations[url]);
   }
   else {
-    console.log('THE URL',url);
     superagent.get(url)
       .then(data => {
         const geoData = data.body;
@@ -55,17 +56,6 @@ function locationHandler(request,response) {
   }
 }
 
-
-
-function tableHandler(req, res) {
-  let SQL = `SELECT * FROM location_table`;
-  client.query(SQL)
-    .then( results => {
-      res.status(200).json(results.rows);
-    })
-    .catch( err => console.err(err));
-}
-
 function Location(query, geoData) {
   this.search_query = query;
   this.formatted_query = geoData.results[0].formatted_address;
@@ -74,6 +64,7 @@ function Location(query, geoData) {
   this.place_id = geoData.results[0].place_id;
 }
 
+///WEATHER
 function weatherHandler(request,response) {
 
   const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
@@ -94,32 +85,72 @@ function Weather(day) {
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
+///
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ name: 'brian ' });
-// });
+///YELP
+function yelpHandler(request,response) {
 
-// GET  localhost:3000/add?first=brian&last=nations
-// app.get('/add', (req, res) => {
-//   let firstName = req.query.first;
-//   let lastName = req.query.last;
-//   let SQL = 'INSERT INTO people (first_name, last_name) VALUES ($1, $2) RETURNING *';
-//   let safeValues = [firstName, lastName];
-//   client.query(SQL, safeValues)
-//     .then(results => {
-//       res.status(200).json(results);
-//     })
-//     .catch(err => console.error(err));
-// });
+  const url = `https://api.yelp.com/v3/businesses/search?latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
 
-// app.get('/people', (req, res) => {
-//   let SQL = 'SELECT * FROM people';
-//   client.query(SQL)
-//     .then(results => {
-//       res.status(200).json(results.rows);
-//     })
-//     .catch(err => console.err(err));
-// })
+  superagent.get(url)
+    .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+    .then( data => {
+      const yelpReviews = data.body.businesses.map(business => {
+        return new Yelp(business);
+      });
+      response.status(200).json(yelpReviews);
+    })
+    .catch( () => {
+      errorHandler(`So sorry, something went wrong. url: ${url}`, request, response);
+    });
+}
+
+function Yelp(business) {
+  this.name = business.name;
+  this.image_url = business.image_url;
+  this.price = business.price;
+  this.rating = business.rating;
+  this.url = business.url;
+}
+///
+
+///MOVIES
+function movieHandler(request,response) {
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${request.query.data}&page=1&include_adult=false`;
+
+  superagent.get(url)
+    .then( data => {
+      const cityMovies = data.body.results.map(movie => {
+        return new Movie(movie);
+      });
+      response.status(200).json(cityMovies);
+    })
+    .catch( () => {
+      errorHandler(`So sorry, something went wrong. url: ${url}`, request, response);
+    });
+}
+
+function Movie(movie) {
+  this.title = movie.title;
+  this.overview = movie.overview;
+  this.average_votes = movie.vote_average;
+  this.image_url = movie.poster_path;
+  this.popularity = movie.popularity;
+  this.released_on = movie.release_date;
+}
+///
+
+///STORAGE
+function tableHandler(req, res) {
+  let SQL = `SELECT * FROM location_table`;
+  client.query(SQL)
+    .then( results => {
+      res.status(200).json(results.rows);
+    })
+    .catch( err => console.err(err));
+}
+///
 
 function notFoundHandler(request,response) {
   response.status(404).send('huh?');
