84f65f385fb3b3d1fd23e7e47e18e2ed1893c25c
egconley <31393715+egconley@users.noreply.github.com>
2019-11-10 15:54:54 -0800

    all 4 APIs rendering correctly, partially set up storage - just location table taking in each new search

--- schema.sql
+++ schema.sql
@@ -2,7 +2,18 @@ DROP TABLE IF EXISTS location_table;
 
 CREATE TABLE location_table (
     id SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
     latitude FLOAT,
     longitude FLOAT,
     place_id VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS weather_table;
+
+CREATE TABLE weather_table (
+    id SERIAL PRIMARY KEY,
+    global_search_query VARCHAR(255),
+    global_place_id VARCHAR(255),
+    forecast VARCHAR(255),
+    time_var VARCHAR(255)
 )
